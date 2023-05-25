a673caa227435637eead4a20e18ce7db1b171f41
MarcosSantosDev <marcosadriano034@gmail.com>
2023-05-25 00:39:46 -0300

    feat: configure stitches

---
+++ src/common/modules/home/home.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+import StitchesLogo from '../../components/context/home/StitchesLogo'
+
+import * as S from './home.styles';
+
+export default function Home() {
+  return (
+    <S.Box css={{ paddingY: '$6' }}>
+      <Head>
+        <title>Use Stitches with Next.js</title>
+      </Head>
+      <S.Container size={{ '@initial': '1', '@bp1': '2' }}>
+        <StitchesLogo />
+        <S.Text as="h1">Hello, from Stitches.</S.Text>
+        <S.Text>
+          For full documentation, visit{' '}
+          <S.Link href="https://stitches.dev">stitches.dev</S.Link>.
+        </S.Text>
+      </S.Container>
+    </S.Box>
+  )
+}
